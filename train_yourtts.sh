7be65cb029ce0a30f81e3548abcda975f0c7492a
Tobias Bollinger <perity98@login01.cluster.bc2.ch>
2023-02-09 15:47:29 +0100

    added tran files

---
+++ train_yourtts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=train_YourTTS     #Name of your job
+#SBATCH --cpus-per-task=4    #Number of cores to reserve
+#SBATCH --mem-per-cpu=32G     #Amount of RAM/core to reserve
+#SBATCH --time=4-00:00:00      #Maximum allocated time
+#SBATCH --qos=1week       #Selected queue to allocate your job
+#SBATCH --partition=a100
+#SBATCH --gres=gpu:4
+#SBATCH --output=log/train_yourtts.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=log/train_yourtts.o%j    #Path and name to the file for the STDERR
+
+#export TF_FORCE_GPU_ALLOW_GROWTH=true
+
+ml CUDA/11.7.0
+ml Miniconda2/4.3.30
+source activate tts
+
+export CUDA_VISIBLE_DEVICES=1,2,3,4
+python -m trainer.distribute --script train_yourtts.py \
+    --restore_path  /scicore/home/graber0001/perity98/.local/share/tts/tts_models--multilingual--multi-dataset--your_tts/model_file.pth
