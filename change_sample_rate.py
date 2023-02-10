152f3d7f9c45a6a1fb34fec390dd2e09ea1c2afe
Tobias Bollinger <tobias.bollinger@gmail.com>
2023-02-10 19:52:47 +0100

    wip

--- change_sample_rate.py
+++ change_sample_rate.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 in_path = "data/swissDial/wavs/"
 out_path = "data/swissDial_1600/wavs/"
 
-for file in tqdm(os.listdir("data/slowsoft/wavs/")):
+for file in tqdm(os.listdir(in_path)):
     audio, sr = sf.read(in_path + file)
     resampled = librosa.resample(audio, orig_sr=sr, target_sr=16000)
     sf.write(out_path + file, audio, 16000)
